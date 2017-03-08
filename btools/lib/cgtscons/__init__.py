0ba5db57b8404cea72ca8cb034f69e332b687148
Mark Diekhans <markd@soe.ucsc.edu>
2017-03-08 15:43:58 -0800

    work

--- btools/lib/cgtscons/__init__.py
+++ btools/lib/cgtscons/__init__.py
@@ -3,29 +3,33 @@ import os
 # library base names
 SONLIB_LIB_NAME = "sonlib"
 CUTEST_LIB_NAME = "cutest"
+CACTUS_LIB_NAME = "cactus"
 
 # special output paths
 TEST_BIN_DIR = "testbin"
 
+# run directories at root, all output is installed from build directories to here.
+RUN_DIR = "#/run"
+# RUN_BIN_DIR = os.path.join(RUN_DIR, "bin")
+# RUN_LIB_DIR = os.path.join(RUN_DIR, "lib")
+# RUN_INCLUDE_DIR = os.path.join(RUN_DIR, "include")
 
-###
-# Code to deal with putting everything in the build directory scons gives very
-# poor control over location of some output files. VariantDir doesn't give the
-# layout we want for object files
-# Setting LIBPREFIX="lib/" handles libraries, but need to build the explictly
-# from objects in different directory, not source.
-###
-def _objectPathBuild(env, libOrProgName, srcFile):
-    "build a object from a source file"
-    # might be an FS object, so conver to string
-    objPath = os.path.join("objs", libOrProgName,
-                           os.path.splitext(os.path.basename(str(srcFile)))[0] + ".o")
-    return  env.StaticObject(target=objPath, source=srcFile)
+##
+# building and linking
+##
+def buildStaticLibrary(env, libBaseName, srcs):
+    """link a static library"""
+    env.StaticLibrary(target="lib" + libBaseName, source=srcs)
 
-def objectPathsBuild(env, libOrProgName, srcFiles):
-    """explictly build objects paths so they end up in
-    VariantDir/objs/libOrProgName"""
-    return [_objectPathBuild(env, libOrProgName, srcFile) for srcFile in srcFiles]
+def linkTestProg(env, progName, srcDir, srcFiles):
+    """link a test program, with specified srcs from srcFiles in srcDir.
+    If srcDir can be none, if srcFiles contains full paths"""
+    if srcDir is not None:
+        srcPaths = [os.path.join(srcDir, sf) for sf in srcFiles]
+    else:
+        srcPaths = srcFiles
+    env.Program(target=os.path.join(TEST_BIN_DIR, progName),
+                source=srcPaths)
 
 
 ##
@@ -40,6 +44,8 @@ def mkRelSymLink(target, source, env):
     # count directories up from target
     numDirs = sourceTail.count("/")
     sourceRel = os.path.join(*((numDirs * ['..']) + [sourceTail]))
+    if os.path.exists(targetAbs):
+        os.unlink(targetAbs)
     os.symlink(sourceRel, targetAbs)
 
 def envRelSymlink(env, source, target):
